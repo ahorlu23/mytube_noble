39346c0ba86bc934008086027b7bb1c1edd10b81
ahorlu23 <nobleahorlu4@gmail.com>
2024-10-12 01:44:44 +0000

    Added some few things

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,12 +11,13 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import MovieIcon from '@mui/icons-material/Movie';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'Noble', icon: <CodeIcon />, },
+  { name: 'Noble', icon: <MovieIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
